9f2f26607ce0b12b970de4668e92fad856fed551
Evelyn-Nicole May <nathaniel.lidstone@outlook.com>
2019-12-28 03:36:18 -0330

    fixed

--- mio.py
+++ mio.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from settings import PREFIX, TOKEN, PRIVATE_SERVER_ID, GENERAL_CHANNEL_ID, DEFAULT_ROLES
+from settings import PREFIX, TOKEN, PRIVATE_SERVER_ID, GENERAL_CHANNEL_ID, DEFAULT_ROLES, BOT_ROLE
 
 bot = commands.Bot(command_prefix=PREFIX)
 bot.remove_command('help')
@@ -18,13 +18,20 @@ async def on_ready():
 @bot.event
 async def on_member_join(member):
     if member.guild.id == PRIVATE_SERVER_ID:
-        for role in DEFAULT_ROLES:
+        if member.bot is False:
+            for role in DEFAULT_ROLES:
+                try:
+                    await member.add_roles(member.guild.get_role(role), atomic=True)
+                except:
+                    pass
+
+            image = discord.File('hi.jpg', filename='hi.jpg', spoiler=False)
+            await member.guild.get_channel(GENERAL_CHANNEL_ID).send(file=image)
+        
+        elif member.bot is True:
             try:
-                await member.add_roles(member.guild.get_role(role), atomic=True)
+                await member.add_roles(member.guild.get_role(BOT_ROLE), atomic=True)
             except:
                 pass
-        
-        image = discord.File('hi.jpg', filename='hi.jpg', spoiler=False)
-        await member.guild.get_channel(GENERAL_CHANNEL_ID).send(file=image)
 
 bot.run(TOKEN)
